a3c86a1bcc8856f6e3431755f890b2d92834e1a6
Duzzyn <eduardonense05@gmail.com>
2023-11-06 19:25:54 -0300

    Projeto previsão do tempo

---
+++ script.js
@@ -0,0 +1,83 @@
+const key = "61639c86aa1ec11054e82d704c895d41";
+
+const cityInput = document.getElementById("input-city")
+const search = document.getElementById("btn-search")
+const showCity = document.getElementById("city")
+const showDegrees = document.getElementById("degrees")
+const showSweather = document.getElementById("sweather")
+const showData = document.getElementById("data")
+
+const alert = document.getElementById("alert")
+
+// functions
+
+async function previsaoDoTempo (city) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br`)
+    if(!response.ok) {
+        throw new Error("Erro de requisição") // ao lançar um error, ou seja:
+        // "throw", as instruções após essa linha, não será executada!
+    }
+    const data = await response.json()            
+    return data;
+}
+
+async function showWeather(city) {
+    const outputHtml = document.getElementById("output-js") 
+    const data = await previsaoDoTempo(city)
+    
+    showCity.innerText = data.name
+    const degreesKelvin = data.main.temp_max
+    degreesConsertion(degreesKelvin)
+    const str = data.weather[0].description
+    conversionUpperCase(str)
+    returnDate()
+    
+    outputHtml.style.display = "flex" // exibe o display depois das inforamações serem
+    // carregadas
+}
+
+function degreesConsertion(degreesKelvin) {
+    degreesCelsius = 273,15
+    const celsius = Math.floor(degreesKelvin - degreesCelsius)
+    showDegrees.innerHTML = `<p>${celsius}°</p>`
+}
+
+function conversionUpperCase(str) {
+    const upperCase = str.charAt(0).toUpperCase() // pega a primeira letra
+    // e transforma em letra maiuscula
+    const arr = str.split("") // separa a string por ""
+    arr.splice(0, 1, upperCase) // substitui o primeiro indice da string do array
+    // depois substitui apenas 1 array e transforma em UpperCase
+    const resultUpperCase = arr.join("") // junta o array pra não ficar separado
+    showSweather.innerText = resultUpperCase // mostra a string na tela
+}
+
+function returnDate () {
+    const date = new Date()
+    const dateOfMonth = date.getDate().toString().padStart(2, "0")
+    const month = (date.getMonth() + 1).toString().padStart(2, "0")
+    const year = date.getFullYear()
+
+    const fullDate = `${dateOfMonth}/${month}/${year}`
+    showData.innerText = fullDate;
+}
+
+function validation() {
+    if(cityInput.value === "") {
+        alert.style.display = "block"
+        alert.innerText = "Não foi possível encontrar a cidade."
+        setTimeout(() => {
+            alert.style.display = "none"
+        }, 3000);
+    }
+}
+
+// events
+
+search.addEventListener("click", (e) => {
+    e.preventDefault()
+    const city = cityInput.value
+
+    validation()
+    showWeather(city)
+})
